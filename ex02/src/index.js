c8304204447ce8f7dd15dd90ff50ab31fb273606
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-10 11:54:46 +0100

    Commenting

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    //Only change code below this line
+    
+    //One-line comment
+
+    /*Multi-line
+    comment*/
+    
+    //Only change code above this line
+}
+
+console.log(main());
+module.exports = main;
