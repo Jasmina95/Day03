21e10950b91f22f27412c648a1946d1dcfb013f1
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-10 13:00:19 +0100

    The Sum of Two Numbers

---
+++ ex08/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    sum = numOne + numTwo;
+    //Only change code above this line
+    return sum;
+}
+
+console.log(main(150, 425));
+module.exports = main;
