4fd34c694804db41d65a6d62f09bcf8cbd77eb70
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-10 13:00:42 +0100

    The Subtract of Two Numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo){
+    //Only change code below this line
+    difference = numOne - numTwo;
+    //Only change code above this line
+    return difference;
+}
+
+console.log(main(425, 120));
+module.exports = main;
