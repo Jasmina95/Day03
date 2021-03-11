37913c4bdeacfdb8502a532ccb5587d9e0574011
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-11 09:31:58 +0100

    Multiplying Two Numbers

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,5 +1,7 @@
 function main(numOne, numTwo){
     //Only change code below this line
+    var numOne = numOne;
+    var numTwo = numTwo;
     var product = numOne * numTwo;
     //Only change code above this line
     return product;
